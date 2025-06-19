eea5dc3cfca7b9d47f46fdea32cd9b6cbd03ec51
Bejetto <bejetto98@gmail.com>
2025-06-19 18:02:36 +0900

    v.0.0.15] feat. Boss 공격 패턴 추가

--- Assets/3.Script/Characters/AIs/Boss/AIBossCharacterManager.cs
+++ Assets/3.Script/Characters/AIs/Boss/AIBossCharacterManager.cs
@@ -1,16 +1,157 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
 namespace SG
 {
-    public class AIBossCharacterManager : MonoBehaviour
+    public class AIBossCharacterManager : AICharacterManager
     {
-        //  GIVE THIS A.I A UNIQUE ID
+        public int bossID = 0;
+        [SerializeField] bool hasBeenDefeated = false;
+        [SerializeField] bool hasBeenAwakened = false;
+        [SerializeField] List<FogWallInteractable> fogWalls;
         //  WHEN THIS A.I IS SPAWNED, CHECK OUR SAVE FILE (DICTIONARY)
         //  IF THE SAVE FILE DOES NOT CONTAIN A BOSS MONSTER WITH THIS I.D ADD IT
         //  IF IT IS PRESENT, CHECK IF THE BOSS HAS BEEN DEFEATED
         //  IF THE BOSS HAS BEEN DEFEATED, DISABLE THIS GAMEOBJECT
         //  IF THE BOSS HAS NOT BEEN DEFEATED, ALLOW THIS OBJECT TO CONTINUE TO BE ACTIVE
+
+        [Header("DEBUG")]
+        [SerializeField] bool wakeBossUp = false;
+
+        protected override void Update()
+        {
+            base.Update();
+
+            if (wakeBossUp)
+            {
+                wakeBossUp = false;
+                WakeBoss();
+            }
+        }
+
+        public override void OnNetworkSpawn()
+        {
+            base.OnNetworkSpawn();
+
+            if (IsServer)
+            {
+                //  IF OUR SAVE DATA DOES NOT CONTAIN INFORMATION ON THIS BOSS, ADD IT NOW
+                if (!WorldSaveGameManager.instance.currentCharacterData.bossesAwakened.ContainsKey(bossID))
+                {
+                    WorldSaveGameManager.instance.currentCharacterData.bossesAwakened.Add(bossID, false);
+                    WorldSaveGameManager.instance.currentCharacterData.bossesDefeated.Add(bossID, false);
+                }
+                //  OTHERWISE, LOAD THE DATA THAT ALREADY EXISTS ON THIS BOSS
+                else
+                {
+                    hasBeenDefeated = WorldSaveGameManager.instance.currentCharacterData.bossesDefeated[bossID];
+                    hasBeenAwakened = WorldSaveGameManager.instance.currentCharacterData.bossesAwakened[bossID];                
+                }
+
+                //  LOCATE FOG WALLS
+                StartCoroutine(GetFogWallsFromWorldObjectManager());
+
+                //  IF THE BOSS HAS BEEN AWAKENED, ENABLE THE FOG WALLS
+                if (hasBeenAwakened)
+                {
+                    for (int i = 0; i < fogWalls.Count; i++)
+                    {
+                        fogWalls[i].isActive.Value = true;
+                    }
+                }
+
+                //  IF THE BOSS HAS BEEN DEFEATED DISABLE THE FOG WALLS
+                if (hasBeenDefeated)
+                {
+                    for (int i = 0; i < fogWalls.Count; i++)
+                    {
+                        fogWalls[i].isActive.Value = false;
+                    }
+
+                    aiCharacterNetworkManager.isActive.Value = false;
+                }
+            }
+        }
+
+        private IEnumerator GetFogWallsFromWorldObjectManager()
+        {
+            while (WorldObjectManager.instance.fogWalls.Count == 0)
+                yield return new WaitForEndOfFrame();
+
+            fogWalls = new List<FogWallInteractable>();
+
+            foreach (var fogWall in WorldObjectManager.instance.fogWalls)
+            {
+                if (fogWall.fogWallID == bossID)
+                    fogWalls.Add(fogWall);
+            }
+        }
+
+        public override IEnumerator ProcessDeathEvent(bool manuallySelectDeathAnimation = false)
+        {
+            if (IsOwner)
+            {
+                characterNetworkManager.currentHealth.Value = 0;
+                isDead.Value = true;
+
+                //  RESET ANY FLAGS HERE THAT NEED TO BE RESET
+                //  NOTHING YET
+
+                //  IF WE ARE NOT GROUNDED, PLAY AN AERIAL DEATH ANIMATION
+
+                if (!manuallySelectDeathAnimation)
+                {
+                    characterAnimatorManager.PlayTargetActionAnimation("Dead_01", true);
+                }
+
+                hasBeenDefeated = true;
+                //  IF OUR SAVE DATA DOES NOT CONTAIN INFORMATION ON THIS BOSS, ADD IT NOW
+                if (!WorldSaveGameManager.instance.currentCharacterData.bossesAwakened.ContainsKey(bossID))
+                {
+                    WorldSaveGameManager.instance.currentCharacterData.bossesAwakened.Add(bossID, true);
+                    WorldSaveGameManager.instance.currentCharacterData.bossesDefeated.Add(bossID, true);
+                }
+                //  OTHERWISE, LOAD THE DATA THAT ALREADY EXISTS ON THIS BOSS
+                else
+                {
+                    WorldSaveGameManager.instance.currentCharacterData.bossesAwakened.Remove(bossID);
+                    WorldSaveGameManager.instance.currentCharacterData.bossesDefeated.Remove(bossID);
+                    WorldSaveGameManager.instance.currentCharacterData.bossesAwakened.Add(bossID, true);
+                    WorldSaveGameManager.instance.currentCharacterData.bossesDefeated.Add(bossID, true);
+                }
+
+                WorldSaveGameManager.instance.SaveGame();
+            }
+
+            //  PLAY SOME DEATH SFX
+
+            yield return new WaitForSeconds(5);
+
+            //  AWARD PLAYERS WITH RUNES
+
+            //  DISABLE CHARACTER
+        }
+
+        public void WakeBoss()
+        {
+            hasBeenAwakened = true;
+
+            if (!WorldSaveGameManager.instance.currentCharacterData.bossesAwakened.ContainsKey(bossID))
+            {
+                WorldSaveGameManager.instance.currentCharacterData.bossesAwakened.Add(bossID, true);
+            }
+            else
+            {
+                WorldSaveGameManager.instance.currentCharacterData.bossesAwakened.Remove(bossID);
+                WorldSaveGameManager.instance.currentCharacterData.bossesAwakened.Add(bossID, true);
+            }
+
+            for (int i = 0; i < fogWalls.Count; i++)
+            {
+                fogWalls[i].isActive.Value = true;
+            }
+        }
     }
 }
